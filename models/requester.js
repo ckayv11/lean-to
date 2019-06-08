2a329933f775afdcc676b1148b1b0ba42f5037e7
Sansevieria <elizabethleighmather@gmail.com>
2019-06-08 10:53:52 -0500

    volunteer testing js added

--- models/requester.js
+++ models/requester.js
@@ -1,20 +1,95 @@
-module.exports = function (sequelize, DataTypes) {
-    var Requester = sequelize.define("Requester", {
-        name: DataTypes.STRING,
-        title: {
+//Form for creating USER account
+
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        nameFirst: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 len: [1]
             }
         },
-        body: {
-            type: DataTypes.TEXT,
+        nameLast: {
+            type: DataTypes.STRING,
             allowNull: false,
-            len: [1]
+            validate: {
+                len: [1]
+            }
         },
-        category: {
-            type: DataTypes.
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        phoneNum: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [10, 12],
+                isNumeric: true
+            },
+        },
+        //////////////////////////// password validation?
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            }
+        },
+        ////////////////////////////
+        birthday: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                isBefore: "2001-06-12",
+                //throw error if after date. User must be older than 18.
+            }
+        },
+        adress1: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+
+            }
+        },
+        adress2: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        city: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        state: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [2]
+            }
+        },
+        zip: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [5],
+                isNumeric: true
+            }
+        },
+        gender: {
+
+        },
+        services: {
+
         }
     });
+    return User;
 };
+
+
+//join services.js and gender.js
--- models/volunteer.js
+++ models/volunteer.js
@@ -1,13 +1,91 @@
+//Form for creating VOLUNTEER account
+
 module.exports = function (sequelize, DataTypes) {
     var Volunteer = sequelize.define("Volunteer", {
-        name: DataTypes.STRING
+        nameFirst: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        nameLast: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        phoneNum: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [10, 12],
+                isNumeric: true
+            },
+        },
+        //////////////////////////// password validation?
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            }
+        },
+        ////////////////////////////
+        birthday: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                isBefore: "2001-06-12",
+                //throw error if after date. Volunteer must be older than 18.
+            }
+        },
+        adress1: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+
+            }
+        },
+        adress2: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        city: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        state: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [2]
+            }
+        },
+        zip: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [5],
+                isNumeric: true
+            }
+        }
     });
 
-    // associate Volunteer with 
-    Volunteer.associate = function (models) {
-        Volunteer.hasMany(models.Requester, {
-            onDelete: "cascade"
-        });
-    };
+
     return Volunteer;
 };
+
+
+//join services.js and gender.js
